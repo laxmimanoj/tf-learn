6c3dcc8e100113d6c9fede4686f62f9bc6ad99f5
Manoj Poonati <manojpoonati@gmail.com>
2022-02-27 14:30:34 -0600

    resource group creation script

---
+++ az/learn/app-azlearn-api-prod-01/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.98.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-learn-apps-prod-01"
+  location = "South Central US"
+}
