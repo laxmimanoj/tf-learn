244395d89844d448cfc66f57bf9a62220954801f
Manoj Poonati <manojpoonati@gmail.com>
2022-02-27 20:47:48 -0600

    app name changed

--- az/learn/app-azlearn-api-prod-01/main.tf
+++ az/learn/app-azlearn-api-prod-01/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_app_service_plan" "plan" {
 }
 
 resource "azurerm_app_service" "app" {
-  name                = "app-azlearn-api-prod-01"
+  name                = "app-azlearn-prod-01"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.plan.id
