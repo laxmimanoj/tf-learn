c5ea1e7f431cdda4c164a8ae4514a24760a6981b
Manoj Poonati <manojpoonati@gmail.com>
2022-02-27 18:54:04 -0600

    added features block

--- az/learn/app-azlearn-api-prod-01/main.tf
+++ az/learn/app-azlearn-api-prod-01/main.tf
@@ -6,7 +6,20 @@ terraform {
     }
   }
 }
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.98.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {
+    
+  }
+}
 resource "azurerm_resource_group" "rg" {
   name     = "rg-learn-apps-prod-01"
   location = "South Central US"
